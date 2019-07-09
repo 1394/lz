bc2843dbcbf90342242944cb0fffe02ae444f329
dmitri.leto <leto1394@gmail.com>
2019-07-09 17:17:37 +0300

    upd

---
+++ test.js
@@ -0,0 +1,15 @@
+const Lz = require('./lz')
+
+const memoryUsage = Lz.seq(process.memoryUsage)
+
+const r = new Lz(memoryUsage)
+r.map(val => {
+  return new Lz(val)
+    .find(el => el.heapUsed)
+    .mapvals(el => `${(el / 1024).toFixed(2)} kB`)
+    .eval()
+})
+
+r.map(el => el.heapUsed).take(20).eval(console.log)
+
+process.exit(0)
